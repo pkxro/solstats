5ce03e38f1dcbb0e8962d71abeef63bcc67830ac
pkxro <solponomarev@gmail.com>
2024-11-24 11:29:22 -0500

    nit: remove banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
-import { RootProvider } from 'fumadocs-ui/provider';
-import 'fumadocs-ui/style.css';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
-import { Banner } from 'fumadocs-ui/components/banner';
-
+import { RootProvider } from "fumadocs-ui/provider";
+import "fumadocs-ui/style.css";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import { Banner } from "fumadocs-ui/components/banner";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -14,15 +13,12 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
         }}
       >
-        <RootProvider>
-        <Banner variant="rainbow">All data is provided as is. Resources are not formally approved or pushed by the Solana Foundation. NFA / DYOR</Banner>
-          {children}
-          </RootProvider>
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
